c690d2975f865e7daa5083255e10b3eac333f98c
diPencil <dipencilcom@gmail.com>
2026-03-11 10:06:28 +0200

    fix: enforce standalone production build and disable trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
-  trailingSlash: true,
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
   },
+  // منع أي مشاكل في التوجيه
+  trailingSlash: false,
 }
 
 export default nextConfig
